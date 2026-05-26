9683dc65c1c4ca71026a92652e0f852130381eff
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-27 00:23:08 +0100

    Add files via upload

--- starttime.py
+++ starttime.py
@@ -1,6 +1,7 @@
 import threading
 import time
-
+import tkinter as tk
+from tkinter import ttk
 
 class stimes:
     def __init__(self,sleeps:float,subs):
@@ -20,3 +21,29 @@ class stimes:
 
     def sstops(self):
         self.stops=True
+
+class myapps:
+    def __init__(self,root:tk.Tk):
+        self.root=root
+        self.root.title("timer library")
+        self.root.geometry("640x480")
+        self.root.configure(background="black")
+        self.process=ttk.Progressbar(variable=0,value=0,orient="horizontal")
+        self.process.pack(padx=10,pady=10)
+        self.value=0
+        self.time= stimes(0.50,self.hello)
+    def hello(self):
+        self.process.step(3)
+        self.value=self.value+3
+        print(self.value)
+        if self.value>100:
+            self.value=100
+            self.time.sstops()
+        
+        
+
+
+root=tk.Tk()
+apps=myapps(root)
+root.mainloop()
+apps.time.sstops()
